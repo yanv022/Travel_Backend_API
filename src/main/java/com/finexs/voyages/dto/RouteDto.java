bcfa5be52ce5a8ea6c9cb0353f00438e4877b87d
yann ndemmeze <yann.abel.ndemmeze.vougue@mni.thm.de>
2026-02-08 17:38:50 +0100

    update date pattern

--- src/main/java/com/finexs/voyages/dto/RouteDto.java
+++ src/main/java/com/finexs/voyages/dto/RouteDto.java
@@ -17,10 +17,10 @@ public class RouteDto {
     private String departureCity;
     private String arrivalCity;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime departureTime;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime arrivalTime;
 
     private Integer duration;
